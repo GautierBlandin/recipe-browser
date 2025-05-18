c58eaf916da0bd9381c1e6c7c07e75cfe5850578
Gautier <gautier.blandin@gmail.com>
2025-05-18 12:03:11 +0200

    add desktop app

---
+++ apps/desktop/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@recipe-browser/desktop" />
+    </div>
+  );
+}
+
+export default App;
