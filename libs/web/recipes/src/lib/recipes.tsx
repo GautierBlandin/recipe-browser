12d51496ae721ac31597aa88ac4eaad6b0bee8d1
Gautier <gautier.blandin@gmail.com>
2025-05-24 00:52:16 +0200

    only export the recipe routes

--- libs/web/recipes/src/lib/recipes.tsx
+++ libs/web/recipes/src/lib/recipes.tsx
@@ -1,17 +1,9 @@
-import { Recipe } from './models/recipe';
 import { RecipesList } from './recipes-list';
+import { RecipesRepository } from './recipes.repository';
 
-const hardcodedRecipes: Recipe[] = [
-  { id: '1', name: 'Roasted Chicken' },
-  { id: '2', name: 'Pasta Pesto' },
-  { id: '3', name: 'Pasta Carbonara' },
-  { id: '4', name: 'Pasta Salmon' },
-  { id: '5', name: 'Beef Stir Fry' },
-  { id: '6', name: 'Chicken Curry' },
-  { id: '7', name: 'Mediterranean Salad' },
-  { id: '8', name: 'Mushroom Risotto' },
-];
+const recipesRepository = new RecipesRepository();
 
 export function Recipes() {
-  return <RecipesList recipes={hardcodedRecipes} />;
+  const recipes = recipesRepository.getAllRecipes();
+  return <RecipesList recipes={recipes} />;
 }
